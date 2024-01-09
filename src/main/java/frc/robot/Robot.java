6ecf588d0ad4c2eb8d9f8c94451b4162e306e52d
excalibur <ExcaliburFRC@gmail.com>
2024-01-09 12:50:04 +0200

    fix isBlueAlliance method

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -30,7 +30,6 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    AllianceUtilities.isBlueAlliance = DriverStation.getAlliance().get().equals(DriverStation.Alliance.Blue);
   }
 
   /**
