c4e7928c40ce9a307895d78fb7e1ddb1226e40a8
excalibur <ExcaliburFRC@gmail.com>
2024-01-09 15:25:50 +0200

    merge RobotContainer & Robot

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,20 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.util.AllianceUtilities;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import frc.robot.subsystems.LEDs;
+import frc.robot.subsystems.swerve.Swerve;
+
+import static edu.wpi.first.math.MathUtil.applyDeadband;
+import static frc.lib.Color.Colors.WHITE;
+import static frc.robot.subsystems.LEDs.LEDPattern.SOLID;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,19 +26,46 @@ import frc.robot.util.AllianceUtilities;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+  // subsystems
+  private final Swerve swerve = new Swerve();
+  private final LEDs leds = LEDs.getInstance();
 
-  private RobotContainer m_robotContainer;
+  // controllers
+  private final CommandPS4Controller controller = new CommandPS4Controller(0);
 
+  // commands
+  private final Command m_autonomousCommand = Commands.none(); // insert autonomous command here
+
+  // bindings
+  private void configureBindings() {
+    swerve.setDefaultCommand(
+            swerve.driveSwerveCommand(
+                    () -> applyDeadband(-controller.getLeftY(), 0.07),
+                    () -> applyDeadband(-controller.getLeftX(), 0.07),
+                    () -> applyDeadband(-controller.getRightX(), 0.07),
+                    controller.L2().negate(),
+                    controller::getR2Axis));
+
+    controller.touchpad().whileTrue(toggleMotorsIdleMode().alongWith(leds.applyPatternCommand(SOLID, WHITE.color)));
+    controller.PS().onTrue(swerve.setOdometryPositionCommand(new Pose2d(0, 0, new Rotation2d(0))));
+  }
+
+  // methods
+  public Command toggleMotorsIdleMode() {
+    return new ParallelCommandGroup(
+            swerve.toggleIdleModeCommand()
+            // add other subsystems here
+    );
+  }
+
+  // Robot methods
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    configureBindings();
   }
 
   /**
@@ -48,58 +84,39 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().run();
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {}
 
   @Override
   public void disabledPeriodic() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
+    m_autonomousCommand.schedule();
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
+    m_autonomousCommand.cancel();
   }
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {}
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 
-  /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {}
 
-  /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {}
 }
