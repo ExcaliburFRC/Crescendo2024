11cbd71d636c82a8f21b5818e345145fc16e8eb5
Team <yehudarothstein@gmail.com>
2024-01-19 13:24:06 +0200

    Added Interpolation For ShootFromDistanceCommand and added variables in Constants

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -11,6 +12,9 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.lib.Neo;
+import edu.wpi.first.math.interpolation.Interpolator;
+
+import edu.wpi.first.math.interpolation.InverseInterpolator;
 
 import java.util.function.DoubleSupplier;
 import static frc.robot.Constants.ShooterConstants.*;
@@ -20,7 +24,6 @@ public class Shooter extends SubsystemBase {
     private final Neo shooterFollower = new Neo(FOLLOWER_SHOOTER_MOTOR_ID);
 
     private final Neo linear = new Neo(LEADER_LINEAR_MOTOR_ID);
-    private final Neo linearFollower = new Neo(FOLLOWER_LINEAR_MOTOR_ID);
     private final DigitalInput beamBreak = new DigitalInput(SHOOTER_BEAMBREAK_CHANNEL);
     public final Trigger noteTrigger = new Trigger(beamBreak::get);
 
@@ -31,11 +34,13 @@ public class Shooter extends SubsystemBase {
 
     private final SimpleMotorFeedforward shooterFF = new SimpleMotorFeedforward(SHOOTER_FF.ks, SHOOTER_FF.kv, SHOOTER_FF.ka);
     private final PIDController linearPID = new PIDController(LINEAR_PID.kp, LINEAR_PID.ki, LINEAR_PID.kd);
+    private final InterpolatingTreeMap interpolate = new InterpolatingTreeMap(InverseInterpolator.forDouble(), Interpolator.forDouble());
 
 
 
     public Shooter(){
         shooterFollower.follow(shooter, true);
+        Neo linearFollower = new Neo(FOLLOWER_LINEAR_MOTOR_ID);
         linearFollower.follow(linear);
     }
 
@@ -87,12 +92,19 @@ public class Shooter extends SubsystemBase {
     }
 
 
+
+
+
+
     public Command ShootFromDistanceCommand(DoubleSupplier distance)  {
         return new RunCommand(
                 ()-> {
+                    interpolate.put(MIN_SHOOTING_DISTANCE, MIN_SHOOTING_SPEED);
+                    interpolate.put(MAX_SHOOTING_DISTANCE, MIN_SHOOTING_SPEED);
             double pid  = shooterPID.calculate(distance.getAsDouble(), shooter.getVelocity());
             double ff = shooterFF.calculate(distance.getAsDouble(), 0);
-            shooter.set(pid+ff);
+            shooter.set((double)interpolate.get(SET_SHOOTING_RANGE)+pid+ff);
+
         },
                 this).until(noteTrigger.negate()).andThen( () -> StopMotorCommand(shooter));
 
