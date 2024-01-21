8cfa316506d369fe09b40ba18c40b31c86ad7706
Team <yehudarothstein@gmail.com>
2024-01-21 20:07:45 +0200

    Added ManualShooterCommand and PrepShooterCommand For Init Commit

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,16 +4,12 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.lib.Neo;
-
 import java.util.function.DoubleSupplier;
 import static frc.robot.Constants.ShooterConstants.*;
 
@@ -22,8 +18,7 @@ public class Shooter extends SubsystemBase {
     private final Neo shooterFollower = new Neo(FOLLOWER_SHOOTER_MOTOR_ID);
     private final Neo linearFollower = new Neo(FOLLOWER_LINEAR_MOTOR_ID);
     private final Neo linear = new Neo(LEADER_LINEAR_MOTOR_ID);
-    private final DigitalInput beamBreak = new DigitalInput(SHOOTER_BEAMBREAK_CHANNEL);
-    public final Trigger noteTrigger = new Trigger(beamBreak::get);
+
 
     public ShuffleboardTab shooterTab = Shuffleboard.getTab("ShooterTab");
     private final GenericEntry shooterSpeed = shooterTab.add("shootSpeedPercent", 0).getEntry();
@@ -33,7 +28,7 @@ public class Shooter extends SubsystemBase {
     private final PIDController linearPID = new PIDController(LINEAR_PID.kp, LINEAR_PID.ki, LINEAR_PID.kd);
     private final InterpolatingDoubleTreeMap interpolate = new InterpolatingDoubleTreeMap();
 
-    public Shooter(){
+    public Shooter() {
         shooterFollower.follow(shooter, true);
         Neo linearFollower = new Neo(FOLLOWER_LINEAR_MOTOR_ID);
         linearFollower.follow(linear);
@@ -41,48 +36,36 @@ public class Shooter extends SubsystemBase {
         interpolate.put(MAX_SHOOTING_DISTANCE, MAX_SHOOTING_RPM);
     }
 
-    private void setLinearSpeed(double speed){
-        setLinearPID(speed);
-    }
-
-    public Command AMPShooter(){
-        return this.runEnd(
-                ()->{
-                    double LinearAmpPID = linearPID.calculate(linear.getVelocity(), LINEAR_SETPOINT);
-                    linear.set(LinearAmpPID);
-
-                    setShooterPID(SHOOTER_AMP_SPEED);
-                },
-                shooter::stopMotor)
-                .andThen(
-                new InstantCommand(()->linear.set(linearPID.calculate(0, linear.getVelocity()))).until(noteTrigger.negate()));
-    }
-
-    public Command setShooterVelocityCommand(){
-        return this.runEnd(
-                ()-> setShooterPID(SETPOINT_SHOT_SPEAKER),
-                shooter::stopMotor).until(noteTrigger.negate());
-    }
-
-    private void setShooterPID(double setpoint) {
+    private void setShooterRPM(double setpoint) {
         double pid = shooterPID.calculate(setpoint, shooter.getVelocity());
         double ff = shooterFF.calculate(setpoint, 0);
         double output = pid + ff;
         shooter.set(output);
     }
-    private void setLinearPID(double setpoint){
+
+    private void setLinearPID(double setpoint) {
         double pid = linearPID.calculate(setpoint, linear.getVelocity());
         linear.set(pid);
+
     }
 
-    public Command StartLinearMotorCommand(DoubleSupplier speed){
-        return new RunCommand(() -> setLinearSpeed(speed.getAsDouble()));
-    }
+//Waiting for LinearState To Be Coded By Ori
+//private void setLinearState(LinearState){pid(state.length)}
 
-    public Command ManualShooterCommand() {
-        return runEnd(
-                ()-> shooter.set(shooterSpeed.getDouble(0)),
-                shooter::stopMotor).until(noteTrigger.negate());
-    }}
+    public Command ManualShooterCommand(DoubleSupplier DutyCycle) {
+        return new RunCommand(() -> {
+            linear.set(DutyCycle.getAsDouble());
+            shooter.set(shooterSpeed.getDouble(0));
+        }, this);
+    };
+
+    public Command PrepShooterCommand(DoubleSupplier amp, DoubleSupplier speaker) {
+        return new RunCommand(() -> {
+            shooter.set(PREP_SHOOTER_SPEED);
+        }, this);
+    };
+
+}
+
 // Path: src/main/java/frc/robot/subsystems/Shooter.java
 
