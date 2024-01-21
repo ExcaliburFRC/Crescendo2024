4cdd170bfce3b42e052d2dbd4e42faba2023295a
Keller6738 <155310889+Keller6738@users.noreply.github.com>
2024-01-21 21:06:22 +0200

    Added shoot methods (#14)
    Co-authored-by: Ori Korngut <ori.korngut@gmail.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -146,18 +146,22 @@ public final class Constants {
 
         public static final Gains SHOOTER_PID = new Gains(0,0,0);
         public static final Gains SHOOTER_FF = new Gains(0,0,0,0);
-        public static final Gains LINEAR_PID = new Gains(0,0,0);
 
         public static final double LINEAR_LENGTH = 0;
+        public static final Gains LINEAR_PID = new Gains(0,0,0);
+
         public static final double LINEAR_SETPOINT = 0;
         public static final double LINEAR_SETPOINT_BACK = 0;
-        public static final double SHOOTER_AMP_SPEED = 0.75;
+
+        public static final double SHOOT_AMP_RPM = 0;
         public static final double MIN_SHOOTING_RPM = 0;
         public static final double MAX_SHOOTING_RPM = 0;
+
         public static final double SETPOINT_SHOOT_SPEAKER = 0;
         public static final double SETPOINT_SHOOT_AMP = 0;
         public static final double SETPOINT_LINEAR_AMP = 0;
 
+        public static final double SHOOT_SPEAKER_FROM_WOOFER_RPM = 0;
         public static final double SETPOINT_SHOT_SPEAKER = 0;
         public static final double MIN_SHOOTING_DISTANCE = 0.0;//TODO: Find Values
         public static final double MAX_SHOOTING_DISTANCE = 0.0; //TODO: Find Values
